ef284274bbdb588ff8be64a9b12c406478d392dc
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-07 19:04:10 +0600

    Category swiper created

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
+import Category from '../Category/Category'
 
 const Home = () => {
   return (
-    <Banner></Banner>
+    <div>
+        <Banner></Banner>
+        <Category></Category>
+    </div>
   )
 }
 
