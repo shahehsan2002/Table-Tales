acecea571b01bcd18e7323d819ea475392e345c9
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-17 20:10:56 +0600

    Create Custom Hook Usemenu To Load Menu Data

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,36 +1,65 @@
+// import { useEffect, useState } from "react";
+// // import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+// import MenuItem from "../../Shared/MenuItem/MenuItem";
+// import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
-import { useEffect, useState } from "react";
-import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+// const PopularMenu = () => {
+//     const [menu] = useState();
+//     const popular = menu?.filter(item => item.category === 'popular');
+//     // const [menu, setMenu] = useState([]);
+//     // useEffect(() => {
+//     //     fetch('menu.json')
+//     //         .then(res => res.json())
+//     //         .then(data => {
+//     //             const popularItems = data.filter(item => item.category === 'popular');
+//     //             setMenu(popularItems)
+//     //         })
+//     // }, [])
+//     return (
+//         <section className="mb-12">
+//             <SectionTitle
+//                 heading="From Our Menu"
+//                 subHeading="Popular Items"
+//             ></SectionTitle>
+//             <div className="grid md:grid-cols-2 gap-10">
+//                 {
+//                     popular.map(item => <MenuItem
+//                         key={item._id}
+//                         item={item}
+//                     ></MenuItem>)
+//                 }
+//             </div>
+//             <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
+//         </section>
+//     );
+// };
+
+// export default PopularMenu;
+
 import MenuItem from "../../Shared/MenuItem/MenuItem";
-
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'popular');
-                setMenu(popularItems)
-            })
-    }, [])
-    return (
-        <section className="mb-12">
-            <SectionTitle
-                heading="From Our Menu"
-                subHeading="Popular Items"
-            ></SectionTitle>
-            <div className="grid md:grid-cols-2 gap-10">
-                {
-                    menu.map(item => <MenuItem
-                        key={item._id}
-                        item={item}
-                    ></MenuItem>)
-                }
-            </div>
-            <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
-        </section>
-    );
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
+
+  return (
+    <section className="mb-12">
+      <SectionTitle
+        heading="From Our Menu"
+        subHeading="Popular Items"
+      ></SectionTitle>
+      <div className="grid md:grid-cols-2 gap-10">
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <button className="btn btn-outline border-0 border-b-4 mt-4">
+        View Full Menu
+      </button>
+    </section>
+  );
 };
 
 export default PopularMenu;
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../shared/Cover/Cover";
-import menuImg from '../../../assets/menu/banner3.jpg';
-
+import menuImg from "../../../assets/menu/banner3.jpg";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const Menu = () => {
   return (
@@ -9,10 +9,15 @@ const Menu = () => {
       <Helmet>
         <title>Menu | Table Tales</title>
       </Helmet>
-      <Cover img={menuImg} title='OUR MENU' ></Cover>
+      <Cover img={menuImg} title="OUR MENU"></Cover>
+      {/* main cover */}
+      <SectionTitle
+        subHeading="Don't Miss"
+        heading="Today's Offer"
+      ></SectionTitle>
+      
     </div>
   );
 };
 
 export default Menu;
-
